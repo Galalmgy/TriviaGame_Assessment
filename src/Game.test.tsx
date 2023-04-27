2cb6f01b5d26551260f8502b29b2107e1c2e49e9
galalmgy <galalmgy@gmail.com>
2023-04-27 13:49:30 +0200

    updating tests

--- src/Game.test.tsx
+++ src/Game.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Game from "./components/TriviaGame";
+
+import AnswerForm from "./components/TriviaGame";
 
 jest.setTimeout(30000);
 
@@ -9,5 +11,16 @@ describe("Game component", () => {
     render(<Game />);
     const headerElement = screen.getByText(/Trivia Game/i);
     expect(headerElement).toBeInTheDocument();
+  });
+  test("renders the submit button", () => {
+    render(<Game />);
+    const submitButton = screen.getByRole("button", { name: /Submit/i });
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test("renders the answer input", () => {
+    render(<AnswerForm />);
+    const answerInput = screen.getByLabelText(/Enter your answer/i);
+    expect(answerInput).toBeInTheDocument();
   });
 });
